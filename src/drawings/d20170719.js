a8d36afb32651ebc87f059b5a239a228a8befc83
Philip Harrell <alightedlamp@gmail.com>
2017-08-01 08:27:41 -0500

    module-ize

--- src/drawings/d20170719.js
+++ src/drawings/d20170719.js
@@ -3,71 +3,60 @@ import React, { Component } from 'react';
 
 import Drawing from '../components/Drawing';
 
-class d20170719 extends Component {
-    constructor() {
-        super();
+const d20170719 = function() {
+    const canvas = document.querySelector("canvas");
+    const cx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-        this.canvas = document.querySelector("canvas");
-        this.cx = this.canvas.getContext("2d");
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+    function getRandomBrightColor() {
+        let hue = Math.floor(Math.random() * 360);
+        return `hsl(${hue}, 100%, 50%)`
     }
-    componentDidMount() {
-        function getRandomBrightColor() {
-            let hue = Math.floor(Math.random() * 360);
-            return `hsl(${hue}, 100%, 50%)`
-        }
-        function getRandomDarkColor() {
-            let hue = Math.floor(Math.random() * 360);
-            return `hsl(${hue}, 100%, 25%)`
-        }
-
-        const W = this.canvas.width;
-        const H = this.canvas.height;
-
-        this.canvas.style.backgroundColor =  '#012';
-
-        let x = 0;
-        let y = 0;
-
-        let hue = 300;
-
-        function drawLine() {
-            this.cx.beginPath();
-            this.cx.strokeStyle = getRandomDarkColor();
-            this.cx.lineWidth = 5;
-            this.cx.moveTo(x, y);
-            this.cx.lineTo(window.innerWidth, y);
-            this.cx.stroke();
-
-            y += 10;
-
-            if (hue > 359) {
-                hue = 0;
-            }
-            else {
-                hue += 20;
-            }
-        }
-
-        function drawRect(e) {
-            let x = e.offsetX;
-            let y = e.offsetY;
-
-            this.cx.fillStyle = getRandomBrightColor();;
-            this.cx.fillRect(x, y, 100, 50);
-        }
-
-        setInterval(drawLine, 1000)
-
-        this.canvas.addEventListener('mousemove', drawRect);
+    function getRandomDarkColor() {
+        let hue = Math.floor(Math.random() * 360);
+        return `hsl(${hue}, 100%, 25%)`
     }
 
-  render() {
-    return(
-      <Drawing />
-    )
-  }
+    const W = canvas.width;
+    const H = canvas.height;
+
+    canvas.style.backgroundColor =  '#012';
+
+    let x = 0;
+    let y = 0;
+
+    let hue = 300;
+
+    function drawLine() {
+        cx.beginPath();
+        cx.strokeStyle = getRandomDarkColor();
+        cx.lineWidth = 5;
+        cx.moveTo(x, y);
+        cx.lineTo(window.innerWidth, y);
+        cx.stroke();
+
+        y += 10;
+
+        if (hue > 359) {
+            hue = 0;
+        }
+        else {
+            hue += 20;
+        }
+    }
+
+    function drawRect(e) {
+        let x = e.offsetX;
+        let y = e.offsetY;
+
+        cx.fillStyle = getRandomBrightColor();;
+        cx.fillRect(x, y, 100, 50);
+    }
+
+    setInterval(drawLine, 1000)
+
+    canvas.addEventListener('mousemove', drawRect);
 }
 
 export default d20170719;
--- src/drawings/d20170722.js
+++ src/drawings/d20170722.js
@@ -1,90 +1,75 @@
 // 20170722 -- PIXELISM
-import React, { Component } from 'react';
-
-import Drawing from '../components/Drawing';
-
 import getHeight from '../modules/getHeight';
 import getWidth from '../modules/getWidth';
 
-class d20170722 extends Component {
-    constructor() {
-        super();
+const d20170722 = function() {
+    const canvas = document.querySelector("canvas");
+    const cx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-        this.canvas = document.querySelector("canvas");
-        this.cx = this.canvas.getContext("2d");
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-    }
-    componentDidMount() {
-        let hue = 0;
-        let saturation = 50;
-        let lightness = 50;
+    let hue = 0;
+    let saturation = 50;
+    let lightness = 50;
 
-        let x = 0;
-        let y = 0;
-        let w = 20;
-        let h = 20;
+    let x = 0;
+    let y = 0;
+    let w = 20;
+    let h = 20;
 
-        let isDrawing = false;
-        let brushWidth = 20;
-        let lastX = 0;
-        let lastY = 0;
+    let isDrawing = false;
+    let brushWidth = 20;
+    let lastX = 0;
+    let lastY = 0;
 
-        let currentColor = '';
-        let lineColor = '';
+    let currentColor = '';
+    let lineColor = '';
 
-        function setNewOrigin(e) {
-            x = e.offsetX;
-            y = e.offsetY;
-            this.cx.moveTo(x, y);
-        }
-
-        setInterval(function() {
-            if (hue > 360) hue = 0;
-            hue++;
-            currentColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-            this.canvas.style.backgroundColor = currentColor;
-        }, 100);
-
-        setInterval(function() {
-            if (x > getWidth()) x = 0;
-            x++;
-
-            if (y > getHeight()) y = 0;
-            y++;
-
-            this.cx.fillStyle = `hsl(${hue/2}, ${saturation}%, ${lightness}%`;
-            this.cx.fillRect(x, y, w, h);
-        }, 1);
-
-        function draw(e) {
-            if (!isDrawing) return;
-
-            this.cx.beginPath();
-            this.cx.strokeStyle = lineColor;
-            this.cx.lineWidth = brushWidth;;
-            this.cx.moveTo(lastX, lastY);
-            this.cx.lineTo(e.offsetX, e.offsetY);
-            this.cx.stroke();
-            [lastX, lastY] = [e.offsetX, e.offsetY];
-        }
-
-        this.canvas.addEventListener('mousedown', function(e) {
-            isDrawing = true;
-            lineColor = currentColor;
-            [lastX, lastY] = [e.offsetX, e.offsetY];
-            setNewOrigin(e);
-        });
-        this.canvas.addEventListener('mousemove', draw);
-        this.canvas.addEventListener('mouseup', () => isDrawing = false);
-        this.canvas.addEventListener('mouseout', () => isDrawing = false);
+    function setNewOrigin(e) {
+        x = e.offsetX;
+        y = e.offsetY;
+        cx.moveTo(x, y);
     }
 
-  render() {
-    return(
-      <Drawing />
-    )
-  }
+    setInterval(function() {
+        if (hue > 360) hue = 0;
+        hue++;
+        currentColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+        canvas.style.backgroundColor = currentColor;
+    }, 100);
+
+    setInterval(function() {
+        if (x > getWidth()) x = 0;
+        x++;
+
+        if (y > getHeight()) y = 0;
+        y++;
+
+        cx.fillStyle = `hsl(${hue/2}, ${saturation}%, ${lightness}%`;
+        cx.fillRect(x, y, w, h);
+    }, 1);
+
+    function draw(e) {
+        if (!isDrawing) return;
+
+        cx.beginPath();
+        cx.strokeStyle = lineColor;
+        cx.lineWidth = brushWidth;;
+        cx.moveTo(lastX, lastY);
+        cx.lineTo(e.offsetX, e.offsetY);
+        cx.stroke();
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
+
+    canvas.addEventListener('mousedown', function(e) {
+        isDrawing = true;
+        lineColor = currentColor;
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+        setNewOrigin(e);
+    });
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', () => isDrawing = false);
+    canvas.addEventListener('mouseout', () => isDrawing = false);
 }
 
 export default d20170722;
